34646736dc3e800a3cc3b6935cd2137f07fac4bb
Eren <erendenek00@gmail.com>
2021-10-27 09:20:00 +0300

    Update button.c

--- buttonCallback/button.c
+++ buttonCallback/button.c
@@ -3,19 +3,23 @@
 
 BUTTON_ITEM_TS myButton;
 
-void buttonCreate(BUTTON_ITEM_TS* button_item, char* _buttonName, BUTTON_EVENT_TYPE_TE _buttonEventType, buttonEvent_t _buttonEventCallback)
+void buttonCreate( 
+BUTTON_ITEM_TS* button_item, 
+const char* _buttonName, 
+const BUTTON_EVENT_TYPE_TE _buttonEventType, 
+const buttonEvent_t _buttonEventCallback )
 {
-    button_item->buttonName 		= _buttonName;
+    button_item->buttonName 			= _buttonName;
     button_item->buttonEventType 	= _buttonEventType;
-    button_item->buttonEventCallback 	= _buttonEventCallback;
+    button_item->buttonEventCallback = _buttonEventCallback;
 
-    printf("===================Buton ozellikleri===================\n");
+	printf("===================Buton ozellikleri===================\n");
     printf("Button name : %s\n", button_item->buttonName), printf("Button event type : %d\n", button_item->buttonEventType);
     printf("===================Buton Simulasyon kullanimi=====================\n");
     printf("Butona tiklama icin 0 degerini giriniz\n");
 }
 
-void buttonControlEvent(BUTTON_ITEM_TS *button_item) //Arayüz tarafından kontrol edilen fonksiyon
+void buttonControlEvent( const BUTTON_ITEM_TS *button_item) //Arayüz tarafından kontrol edilen fonksiyon
 {
     static BUTTON_EVENT_TYPE_TE buttonInput;
 
@@ -30,4 +34,5 @@ void buttonControlEvent(BUTTON_ITEM_TS *button_item) //Arayüz tarafından kontr
         printf("Gecersiz buton girisi\n");
     }
 }
+
 
