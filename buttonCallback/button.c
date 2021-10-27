48db9a37726b2c1b3a5098931f9747b96db65f9b
Eren <erendenek00@gmail.com>
2021-10-27 09:14:49 +0300

    Update button.c

--- buttonCallback/button.c
+++ buttonCallback/button.c
@@ -5,16 +5,17 @@ BUTTON_ITEM_TS myButton;
 
 void buttonCreate(BUTTON_ITEM_TS* button_item, char* _buttonName, BUTTON_EVENT_TYPE_TE _buttonEventType, buttonEvent_t _buttonEventCallback)
 {
-    button_item->buttonName 			= _buttonName;
+    button_item->buttonName 		= _buttonName;
     button_item->buttonEventType 	= _buttonEventType;
-    button_item->buttonEventCallback = _buttonEventCallback;
+    button_item->buttonEventCallback 	= _buttonEventCallback;
 
-	printf("===================Buton ozellikleri===================\n");
+    printf("===================Buton ozellikleri===================\n");
     printf("Button name : %s\n", button_item->buttonName), printf("Button event type : %d\n", button_item->buttonEventType);
     printf("===================Buton Simulasyon kullanimi=====================\n");
     printf("Butona tiklama icin 0 degerini giriniz\n");
 }
-void buttonControlEvent(BUTTON_ITEM_TS *button_item) //Arayüz tarafýndan kontrol edilen fonksiyon
+
+void buttonControlEvent(BUTTON_ITEM_TS *button_item) //ArayÃ¼z tarafÄ±ndan kontrol edilen fonksiyon
 {
     static BUTTON_EVENT_TYPE_TE buttonInput;
 
